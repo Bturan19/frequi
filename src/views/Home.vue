23541c65209e89d4123631bd5233ab1f3b1dd481
Bturan19 <bozkurtturanyigit@gmail.com>
2021-07-30 09:59:40 +0300

    changed some names

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <!-- <img alt="Freqtrade logo" src="../assets/freqtrade-logo.png" width="450px" class="my-5" /> -->
     <div alt="Freqtrade logo" class="logo-svg my-5 mx-auto" />
     <div>
-      <h1>Welcome to the Freqtrade UI</h1>
+      <h1>Welcome to the Coinoner UI</h1>
     </div>
     <div>This page allows you to control your trading bot.</div>
     <br />
